a7f7282ef30472b15c1b5f2f1275025fc6531d85
rmunjewar <riddhismunjewar@gmail.com>
2025-04-12 15:32:14 -0500

    organized files for testing

---
+++ choochoo/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
