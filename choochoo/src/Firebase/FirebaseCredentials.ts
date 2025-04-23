1d9a819c343420a68e0a2b203676914c4a924ca8
cdelzell <claradelzell@gmail.com>
2025-04-23 07:42:15 -0500

    added local .env file!

--- choochoo/src/Firebase/FirebaseCredentials.ts
+++ choochoo/src/Firebase/FirebaseCredentials.ts
@@ -1,16 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, push, DatabaseReference } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, DatabaseReference } from "firebase/database";
 
-// Firebase Credentials, do not post or share, Ty likes not owing google money
+// Firebase Credentials, linked to local .env file
 export const firebaseConfig = {
-  apiKey: "AIzaSyCno9yVTIHQUzvciQs5tnjvWSnX-JewSYQ",
-  authDomain: "the-conductors.firebaseapp.com",
-  databaseURL: "https://the-conductors-default-rtdb.firebaseio.com",
-  projectId: "the-conductors",
-  storageBucket: "the-conductors.firebasestorage.app",
-  messagingSenderId: "552591104221",
-  appId: "1:552591104221:web:12d6209a67fbb26caf2334",
-  measurementId: "G-VZ49VGKG0X"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectID,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 export const app = initializeApp(firebaseConfig);
